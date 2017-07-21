0783244407e9f8c1dbdb1065dee6c68b1ae3a180
kimlan nguyen <kimlan1510@yahoo.com>
2017-07-21 09:56:46 -0700

    Add routerLink to app.component. Create App.routing, locate.component and import LocateCompoent to app.routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LocateComponent } from './locate/locate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocateComponent
   ],
   imports: [
     BrowserModule
